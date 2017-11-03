e86d0cb265cc64c591a1f489ba40b7b9b0bedfdc
frank-ye <frank.jing.ye@gmail.com>
2017-11-02 23:53:45 -0400

    Lab 3 is WORKINGgit add -u .git add -u .git add -u .!

--- g08_lab3/lpm_compare1.vhd
+++ g08_lab3/lpm_compare1.vhd
@@ -71,7 +71,7 @@ ARCHITECTURE SYN OF lpm_compare1 IS
 	END COMPONENT;
 
 BEGIN
-	sub_wire1_bv(25 DOWNTO 0) <= "11100110101100100111111111";
+	sub_wire1_bv(25 DOWNTO 0) <= "00100110001001011001111111";
 	sub_wire1    <= To_stdlogicvector(sub_wire1_bv);
 	aleb    <= sub_wire0;
 
@@ -104,7 +104,7 @@ END SYN;
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
 -- Retrieval info: PRIVATE: LPM_PIPELINE NUMERIC "1"
 -- Retrieval info: PRIVATE: Latency NUMERIC "0"
--- Retrieval info: PRIVATE: PortBValue NUMERIC "999999999"
+-- Retrieval info: PRIVATE: PortBValue NUMERIC "9999999"
 -- Retrieval info: PRIVATE: Radix NUMERIC "10"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SignedCompare NUMERIC "0"
@@ -121,7 +121,7 @@ END SYN;
 -- Retrieval info: USED_PORT: aleb 0 0 0 0 OUTPUT NODEFVAL "aleb"
 -- Retrieval info: USED_PORT: dataa 0 0 26 0 INPUT NODEFVAL "dataa[25..0]"
 -- Retrieval info: CONNECT: @dataa 0 0 26 0 dataa 0 0 26 0
--- Retrieval info: CONNECT: @datab 0 0 26 0 999999999 0 0 26 0
+-- Retrieval info: CONNECT: @datab 0 0 26 0 9999999 0 0 26 0
 -- Retrieval info: CONNECT: aleb 0 0 0 0 @aleb 0 0 0 0
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_compare1.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_compare1.inc FALSE
