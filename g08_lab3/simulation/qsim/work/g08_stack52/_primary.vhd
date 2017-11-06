f69cfb7059004ef0bd3744d4eb804533181e40ec
frank-ye <frank.jing.ye@gmail.com>
2017-11-06 15:05:41 -0500

    Lab 3 slight changes

--- g08_lab3/simulation/qsim/work/g08_stack52/_primary.vhd
+++ g08_lab3/simulation/qsim/work/g08_stack52/_primary.vhd
@@ -2,6 +2,10 @@ library verilog;
 use verilog.vl_types.all;
 entity g08_stack52 is
     port(
+        altera_reserved_tms: in     vl_logic;
+        altera_reserved_tck: in     vl_logic;
+        altera_reserved_tdi: in     vl_logic;
+        altera_reserved_tdo: out    vl_logic;
         EMPTY           : out    vl_logic;
         NUM             : out    vl_logic_vector(5 downto 0);
         MODE            : in     vl_logic_vector(1 downto 0);
